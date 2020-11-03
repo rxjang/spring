e139bd13749aa95fd872646531f7834ce91ac301
rxjang <rxforp@naver.com>
2020-11-03 15:04:30 +0900

    reflection Test

--- springbook/src/main/java/com/bit/springbook/user/service/UserServiceTx.java
+++ springbook/src/main/java/com/bit/springbook/user/service/UserServiceTx.java
@@ -18,19 +18,20 @@ public class UserServiceTx implements UserService {
 	
 	@Override
 	public void add(User user) {
-		userService.add(user);
+		this.userService.add(user);
+		//메소드 구현과 위임
 	}
 	
 	@Override
 	public void upgradeLevels() {
 		TransactionStatus status=this.transactionManager.getTransaction(new DefaultTransactionDefinition());
-		try {
-			userService.upgradeLevels();
+		try {//부가기능 수행
+			userService.upgradeLevels();//위임
 			
 			this.transactionManager.commit(status);
 		}catch(RuntimeException e) {
 			this.transactionManager.rollback(status);
-			throw e;
+			throw e;//부가기능 수행
 		}
 	}
 	//DI받은 UserService 오브젝트에 모든 기능을 위임한다.
