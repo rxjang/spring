89f718cf3a9fbacd5907d39b222f880e5ef90252
rxjang <rxforp@naver.com>
2020-11-03 22:33:00 +0900

    invoationHandler

--- springbook/src/main/java/com/bit/springbook/learningtest/jdk/UppercaseHandler.java
+++ springbook/src/main/java/com/bit/springbook/learningtest/jdk/UppercaseHandler.java
@@ -4,17 +4,22 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class UppercaseHandler implements InvocationHandler {
-	Hello target;
+	Object target;
 	
-	public UppercaseHandler(Hello target) {
+	public UppercaseHandler(Object target) {
 		this.target=target;
 	}
-	//다이내믹 프록시로부터 전달받은 요청을 다시 타깃 오브젝트에 위임해야 하기 때문에 타깃 오브젝트를 주입받아둔다.
+	//어떤 종류의 인터페이스를 구현한 타깃에도 적용 가능하도록 Object타입으로 수정
 	
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable{
-		String ret=(String)method.invoke(target, args);//타깃으로 위임. 인터페이스의 메소드 호출에 모두 적용된다.
-		return ret.toUpperCase();
+		Object ret=method.invoke(target, args);//타깃으로 위임. 인터페이스의 메소드 호출에 모두 적용된다.
+		if(ret instanceof String && method.getName().startsWith("say")) {
+			return ((String)ret).toUpperCase();
+		}else {
+			return ret;
+		}
+		//호출한 메소드의 리턴 타입이 String인 경우에만 대문자 변경 기능을 적용하도록 수정
 	}
 
 }
