e5c11c20838d575bb4841cc1ffaa628f1f0b65d4
rxjang <rxforp@naver.com>
2020-11-03 15:19:38 +0900

    proxy Test

---
+++ springbook/src/main/java/com/bit/springbook/learningtest/jdk/Hello.java
@@ -0,0 +1,7 @@
+package com.bit.springbook.learningtest.jdk;
+
+public interface Hello {
+	String sayHello(String name);
+	String sayHi(String name);
+	String sayThankYou(String name);
+}
---
+++ springbook/src/test/java/com/bit/springbook/learningtest/jdk/HelloTest.java
@@ -0,0 +1,23 @@
+package com.bit.springbook.learningtest.jdk;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void simpleProxy() {
+		Hello hello=new HelloTarget();
+		assertThat(hello.sayHello("Toby"),is("Hello Toby"));
+		assertThat(hello.sayHi("Toby"),is("Hi Toby"));
+		assertThat(hello.sayThankYou("Toby"),is("Thank you Toby"));
+		
+		Hello proxyHello=new HelloUppercase(new HelloTarget());
+		assertThat(proxyHello.sayHello("Toby"),is("HELLO TOBY"));
+		assertThat(proxyHello.sayHi("Toby"),is("HI TOBY"));
+		assertThat(proxyHello.sayThankYou("Toby"),is("THANK YOU TOBY"));
+	}
+
+}
