9b9c1a1f15bb084c39792f2d8ea63079ce9c326c
rxjang <rxforp@naver.com>
2020-11-13 11:50:08 +0900

    pointcut expression test

--- springbook/src/test/java/com/bit/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
+++ springbook/src/test/java/com/bit/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
@@ -9,7 +9,7 @@ import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 public class PointcutExpressionTest {
 	
 	@Test
-	public void methodSignaturePointcut() throws NoSuchMethodException, SecurityException {
+	public void methodSignaturePointcut() throws Exception {
 		AspectJExpressionPointcut pointcut= new AspectJExpressionPointcut();
 		pointcut.setExpression("execution(public int "+
 				"com.bit.springbook.learningtest.spring.pointcut.Target.minus(int,int) "+
@@ -26,5 +26,26 @@ public class PointcutExpressionTest {
 		//Bean.method()
 		assertThat(pointcut.getClassFilter().matches(Bean.class)&&
 				pointcut.getMethodMatcher().matches(Target.class.getMethod("method"), null),is(false));
+		
+		targetClassPointcutMatches("execution(* *(..))", true, true, true, true, true, true);
+	}
+	
+	public void pointcutMatches(String expression, Boolean expected,Class<?> clazz,
+			String methodName, Class<?>... args) throws Exception {
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression(expression);
+		
+		assertThat(pointcut.getClassFilter().matches(clazz)
+		&& pointcut.getMethodMatcher().matches(clazz.getMethod(methodName,
+				args), null),is(expected));
+	}
+	
+	public void targetClassPointcutMatches(String expression,boolean...expected) throws Exception {
+		pointcutMatches(expression, expected[0],Target.class,"hello");
+		pointcutMatches(expression, expected[1],Target.class,"hello",String.class);
+		pointcutMatches(expression, expected[2],Target.class,"plus",int.class,int.class);
+		pointcutMatches(expression, expected[3],Target.class,"minus",int.class,int.class);
+		pointcutMatches(expression, expected[4],Target.class,"method");
+		pointcutMatches(expression, expected[5],Bean.class,"method");
 	}
 }
