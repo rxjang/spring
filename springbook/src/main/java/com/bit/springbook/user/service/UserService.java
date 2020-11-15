0b78e084869002f00016ef67bb86026f959aaf51
rxjang <rxforp@naver.com>
2020-11-15 21:00:33 +0900

    transaction annotation

--- springbook/src/main/java/com/bit/springbook/user/service/UserService.java
+++ springbook/src/main/java/com/bit/springbook/user/service/UserService.java
@@ -2,14 +2,20 @@ package com.bit.springbook.user.service;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.bit.springbook.user.domain.User;
 
+@Transactional
 public interface UserService {
 	void add(User user);
-	User get(String id);
-	List<User> getAll();
 	void deleteAll();
 	void update(User user);
-	
 	void upgradeLevels();
+	
+	@Transactional(readOnly=true)
+	User get(String id);
+	@Transactional(readOnly=true)
+	List<User> getAll();
+	
 }
