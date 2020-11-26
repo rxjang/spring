fc08c683982fefcec1289eb4df38ede478edad2e
rxjang <rxforp@naver.com>
2020-11-26 13:52:04 +0900

    property source

--- springbook/src/main/java/com/bit/springbook/AppContext.java
+++ springbook/src/main/java/com/bit/springbook/AppContext.java
@@ -3,33 +3,26 @@ package com.bit.springbook;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.oxm.Unmarshaller;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.bit.springbook.AppContext.ProductionAppContext;
 import com.bit.springbook.user.dao.UserDao;
-import com.bit.springbook.user.dao.UserDaoJdbc;
 import com.bit.springbook.user.service.DummyMailSender;
 import com.bit.springbook.user.service.UserService;
-import com.bit.springbook.user.service.UserServiceImpl;
-import com.bit.springbook.user.sqlService.OxmSqlService;
-import com.bit.springbook.user.sqlService.SqlRegistry;
-import com.bit.springbook.user.sqlService.SqlService;
-import com.bit.springbook.user.sqlService.updatable.EmbeddedDbSqlRegistry;
 import com.mysql.cj.jdbc.Driver;
 
 @Configuration
@@ -37,20 +30,28 @@ import com.mysql.cj.jdbc.Driver;
 @EnableTransactionManagement
 @ComponentScan(basePackages = "com.bit.springbook.user")
 @Import(SqlServiceContext.class)
+@PropertySource("/database.properties")
 public class AppContext {
-	@Autowired UserDao userDao;
-	@Autowired UserService userService;
+	@Value("${db.driverClass}") Class<?extends Driver> driverClass;
+	@Value("${db.url}") String url;
+	@Value("${db.username}") String username;
+	@Value("${db.password}") String password;
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer palceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 	
 	@Bean
 	public DataSource dataSource() {
-		SimpleDriverDataSource dataSource=new SimpleDriverDataSource();
+		SimpleDriverDataSource ds = new SimpleDriverDataSource();
 		
-		dataSource.setDriverClass(Driver.class);
-		dataSource.setUrl("jdbc:mysql://localhost/testdb?&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true");
-		dataSource.setUsername("spring");
-		dataSource.setPassword("book");
+		ds.setDriverClass(this.driverClass);
+		ds.setUrl(this.url);
+		ds.setUsername(this.username);
+		ds.setPassword(this.password);
 		
-		return dataSource;
+		return ds;
 	}
 	
 	@Bean
