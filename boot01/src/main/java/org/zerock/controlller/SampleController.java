c42036fb7ff0e4552b8ba22ed3e2e0f9b07476aa
장서현 <rxforp@naver.com>
2020-12-19 20:12:06 +0900

    bean test

---
+++ boot01/src/main/java/org/zerock/controlller/SampleController.java
@@ -0,0 +1,13 @@
+package org.zerock.controlller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello World";
+	}
+}
