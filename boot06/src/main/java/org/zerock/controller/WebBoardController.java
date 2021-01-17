3f6aa64abe0a2fea8e2aef32d916c85322bb340b
장서현 <rxforp@naver.com>
2021-01-18 06:57:36 +0900

    register contents

--- boot06/src/main/java/org/zerock/controller/WebBoardController.java
+++ boot06/src/main/java/org/zerock/controller/WebBoardController.java
@@ -6,7 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.WebBoard;
 import org.zerock.persistence.WebBoardRepository;
 import org.zerock.vo.PageMaker;
@@ -35,5 +38,22 @@ public class WebBoardController {
 		log.info("TOTAL PAGE NUMBER" + result.getTotalPages());
 		
 		model.addAttribute("result", new PageMaker(result));
+	}
+	
+	@GetMapping("/register")
+	public void registerGET(@ModelAttribute("vo") WebBoard vo) {
+		log.info("register get");
+	}
+	
+	@PostMapping("/register")
+	public String registerPOST(@ModelAttribute("vo") WebBoard vo, RedirectAttributes rttr) {
+		
+		log.info("register post");
+		log.info("" + vo);
+		
+		repo.save(vo);
+		rttr.addFlashAttribute("msg","success");
+		
+		return "redirect:/boards/list";
 	}
 }
