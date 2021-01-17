81ef66682fb0eea011d728a61bd250692a602a6d
장서현 <rxforp@naver.com>
2021-01-17 23:29:43 +0900

    Add search sevice

--- boot06/src/main/java/org/zerock/vo/PageVO.java
+++ boot06/src/main/java/org/zerock/vo/PageVO.java
@@ -12,6 +12,26 @@ public class PageVO {
 	private int page;
 	private int size;
 	
+	private String keyword;
+	private String type;
+	
+	public String getKeyword() {
+		return keyword;
+	}
+	
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type=type;
+	}
+	
+	
 	public PageVO() {
 		this.page=1;
 		this.size=DEFAULT_SIZE;
