7b2e5f31c30cdb62ab49788bdac150122a321173
장서현 <rxforp@naver.com>
2021-01-18 20:29:59 +0900

    View Content

--- boot06/src/main/java/org/zerock/controller/WebBoardController.java
+++ boot06/src/main/java/org/zerock/controller/WebBoardController.java
@@ -56,4 +56,12 @@ public class WebBoardController {
 		
 		return "redirect:/boards/list";
 	}
+	
+	@GetMapping("/view")
+	public void view(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model){
+		
+		log.info("BNO: "+bno);
+		
+		repo.findById(bno).ifPresent(board -> model.addAttribute("vo", board));
+	}
 }
