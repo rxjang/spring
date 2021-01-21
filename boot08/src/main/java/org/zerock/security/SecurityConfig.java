91ed152a8b44caf510cb39473169a2a2c35937e4
장서현 <rxforp@naver.com>
2021-01-22 07:19:14 +0900

    Start spring security project

---
+++ boot08/src/main/java/org/zerock/security/SecurityConfig.java
@@ -0,0 +1,14 @@
+package org.zerock.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import lombok.extern.java.Log;
+
+@Log
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	
+
+}
