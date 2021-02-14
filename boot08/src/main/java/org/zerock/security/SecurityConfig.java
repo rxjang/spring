3df125dd93f8052e73c7bb4df1667fd987ff307b
장서현 <rxforp@naver.com>
2021-02-14 11:14:12 +0900

    Remember-me

--- boot08/src/main/java/org/zerock/security/SecurityConfig.java
+++ boot08/src/main/java/org/zerock/security/SecurityConfig.java
@@ -3,18 +3,21 @@ package org.zerock.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import lombok.extern.java.Log;
 
 @Log
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	DataSource dataSource;
 	
 	@Autowired
 	ZerockUsersService zerockUserService;
@@ -38,7 +41,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		//세션 무효화 
 		http.logout().logoutUrl("/logout").invalidateHttpSession(true);
 		
-		http.userDetailsService(zerockUserService);
+//		http.userDetailsService(zerockUserService);
+		
+		http.rememberMe()
+		.key("zerock")
+		.userDetailsService(zerockUserService)
+		.tokenRepository(getJDBCRepository())
+		.tokenValiditySeconds(60*60*24);
+		
+		//초단위 ->  24시간 유지 
 	}
 	
 	@Autowired
@@ -56,6 +67,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 //		.rolePrefix("ROLE_")
 //		.authoritiesByUsernameQuery(query2);
 
+	}
+	
+	private PersistentTokenRepository getJDBCRepository() {
+		
+		JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+		repo.setDataSource(dataSource);
+		return repo;
+		
 	}
 	
 
