057fb2f7d6c9c67deabc0b76abdde0a1e60cdd90
장서현 <rxforp@naver.com>
2021-06-20 21:33:07 +0900

    security test

---
+++ spring-security/demo.spring-security-form/src/test/java/com/security/demo/springsecurityform/account/WithUser.java
@@ -0,0 +1,11 @@
+package com.security.demo.springsecurityform.account;
+
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithMockUser(username = "felix", roles = "USER")
+public @interface WithUser {
+}
