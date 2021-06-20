2f95b019c39d8deb81cab3f4fc569a4efe259858
장서현 <rxforp@naver.com>
2021-06-20 15:46:47 +0900

    passwordEncoder

--- spring-security/demo.spring-security-form/src/main/java/com/security/demo/springsecurityform/Application.java
+++ spring-security/demo.spring-security-form/src/main/java/com/security/demo/springsecurityform/Application.java
@@ -2,9 +2,21 @@ package com.security.demo.springsecurityform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Application {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		// sprint security 5 이전 버전
+		// return NoOpPasswordEncoder.getInstance();
+
+		// 다양한 passwordEncoding 을 지원 => 더 안전
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- spring-security/demo.spring-security-form/src/main/java/com/security/demo/springsecurityform/account/AccountService.java
+++ spring-security/demo.spring-security-form/src/main/java/com/security/demo/springsecurityform/account/AccountService.java
@@ -5,14 +5,17 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService implements UserDetailsService {
 
-    // TODO {noop}123
     @Autowired
     AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -30,7 +33,7 @@ public class AccountService implements UserDetailsService {
 
     public Account createNew(Account account) {
 
-        account.encodePassword();
+        account.encodePassword(passwordEncoder);
         return this.accountRepository.save(account);
     }
 }
