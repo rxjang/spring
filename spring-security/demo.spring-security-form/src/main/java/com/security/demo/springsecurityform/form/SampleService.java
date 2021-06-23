89c9b442feab07f305cd2c2394b2808774496df6
장서현 <rxforp@naver.com>
2021-06-23 22:06:48 +0900

    authentication and SecurityContextHolder

--- spring-security/demo.spring-security-form/src/main/java/com/security/demo/springsecurityform/form/SampleService.java
+++ spring-security/demo.spring-security-form/src/main/java/com/security/demo/springsecurityform/form/SampleService.java
@@ -2,14 +2,19 @@ package com.security.demo.springsecurityform.form;
 
 import com.security.demo.springsecurityform.account.Account;
 import com.security.demo.springsecurityform.account.AccountContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SampleService {
 
     public void dashboard() {
-        Account account = AccountContext.getAccount();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails  = (UserDetails) authentication.getPrincipal();
         System.out.println("================");
-        System.out.println(account.getUsername());
+        System.out.println(authentication);
+        System.out.println(userDetails.getUsername());
     }
 }
