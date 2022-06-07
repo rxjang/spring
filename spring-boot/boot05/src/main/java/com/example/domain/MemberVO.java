7a9d7cfe58e09f80f21cc00634a941a1977d13eb
Seohyun Jang <olllehyun@gmail.com>
2022-06-07 09:17:28 +0900

    chap05: basic thymeleaf

---
+++ spring-boot/boot05/src/main/java/com/example/domain/MemberVO.java
@@ -0,0 +1,21 @@
+package com.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+public class MemberVO {
+
+    private int mno;
+
+    private String mid;
+
+    private String mpw;
+
+    private String mname;
+
+    private Timestamp regdate;
+}
