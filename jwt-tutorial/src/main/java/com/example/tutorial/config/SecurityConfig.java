e78c4a905bbb654cd97af2370f67dea3ad8713b2
장서현 <rxforp@naver.com>
2022-07-01 17:29:51 +0900

    jwt: init jwt tutorial project

---
+++ jwt-tutorial/src/main/java/com/example/tutorial/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.tutorial.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers(
+                        "/h2-console/**",
+                        "favicon.ico");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+    }
+}
