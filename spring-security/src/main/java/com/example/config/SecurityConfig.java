7905ecd7a4b2deadd71ae693bd93d4b46c8e100f
Seohyun Jang <olllehyun@gmail.com>
2022-07-16 16:30:25 +0900

    security: filter chain proxy

--- spring-security/src/main/java/com/example/config/SecurityConfig.java
+++ spring-security/src/main/java/com/example/config/SecurityConfig.java
@@ -1,13 +1,13 @@
 package com.example.config;
 
-import com.example.account.AccountService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
+@Order(Ordered.LOWEST_PRECEDENCE - 50)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
--- spring-security/src/main/java/com/example/form/SampleService.java
+++ spring-security/src/main/java/com/example/form/SampleService.java
@@ -5,6 +5,7 @@ import com.example.account.AccountContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -21,6 +22,10 @@ public class SampleService {
 
         Account account = AccountContext.getAccount();
         System.out.println("============" + account.getUsername());
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        System.out.println(authentication);
+        System.out.println("============= userDetails: " + userDetails.getUsername());
 
     }
 }
