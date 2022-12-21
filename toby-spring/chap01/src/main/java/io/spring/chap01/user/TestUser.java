9fc73effe5520218a80cfe5d6b8603d2ebe12724
Seohyun Jang <olllehyun@gmail.com>
2022-12-21 23:32:11 +0900

    토비의 스프링: 1.1 초난감 DAO

---
+++ toby-spring/chap01/src/main/java/io/spring/chap01/user/TestUser.java
@@ -0,0 +1,28 @@
+package io.spring.chap01.user;
+
+import io.spring.chap01.user.dao.UserDao;
+import io.spring.chap01.user.domain.User;
+
+import java.sql.SQLException;
+
+public class TestUser {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("felix");
+        user.setName("용복");
+        user.setPassword("felixlee");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
