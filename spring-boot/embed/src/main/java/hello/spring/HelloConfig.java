05b0f139a3c4a6d2c760aa599686d947b4a39eb1
Seohyun Jang <olllehyun@gmail.com>
2023-06-19 22:06:29 +0900

    스프링부트-핵심 원리: ch2 스프링부트와 웹서버

--- spring-boot/embed/src/main/java/hello/spring/HelloConfig.java
+++ spring-boot/embed/src/main/java/hello/spring/HelloConfig.java
@@ -2,8 +2,9 @@ package hello.spring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
+//@Configuration
 public class HelloConfig {
 
     @Bean
